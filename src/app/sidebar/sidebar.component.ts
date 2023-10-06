1378e1dfe471543dadde44ebde78dfe3022f81fd
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-06 16:21:20 +0200

    init: essentials workspace

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+export class SidebarComponent {
+}

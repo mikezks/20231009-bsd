0c5266385184d5aa7a06766240d336414ff3d2a4
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-08 15:18:19 +0200

    feat: traditional provider config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,14 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { FlightService } from './flight-search/flight.service';
+import { DefaultFlightService } from './flight-search/default-flight.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()]
+  providers: [
+    provideHttpClient(),
+    {
+      provide: FlightService,
+      useClass: DefaultFlightService,
+    }
+  ]
 };
